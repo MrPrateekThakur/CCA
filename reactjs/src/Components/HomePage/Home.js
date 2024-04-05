31b8967f92612515af8e5583a9de1df895341d04
Prateek Thakur <prateek456thakur@gmail.com>
2024-04-05 15:23:52 +0530

    base react folder

---
+++ reactjs/src/Components/HomePage/Home.js
@@ -0,0 +1,11 @@
+import Banner from "./Banner";
+
+export default function Home() {
+    return <>
+        <div className="container mt-3">
+            <Banner />
+            <hr id="sectionLine"/>
+
+        </div>
+    </>
+}
