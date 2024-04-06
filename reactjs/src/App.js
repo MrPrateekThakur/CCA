ceae399e0486cd92a28350d661585fbe11858022
Panchalji13 <panchalankit21111999@gmail.com>
2024-04-06 17:12:19 +0530

    team added

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -3,10 +3,11 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/HomePage/Home.js';
 import Header from './Components/Header.js';
-
+import Team from './Components/TeamPage/Team.js';
 function App() {
   return <>
     <Header />
+    <Team/>
     {/* <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes> */}
---
+++ reactjs/src/Components/HomePage/Team.js
@@ -0,0 +1,14 @@
+import './Teampage.css';
+
+export default function Team() {
+    return <>
+        <div className="mainContainer">
+            <div className={'titleContainer'}>
+                <div>Welcome!</div>
+            </div>
+            <div>This is the home page.</div>
+            <div className={'buttonContainer'}>
+            </div>
+        </div>
+    </>
+}
